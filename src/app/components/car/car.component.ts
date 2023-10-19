75cc1e1c19cb685eafb507f224ce7724c63743a3
f.nikolaev <f.nikolaev@bpm-cons.ru>
2023-10-19 19:07:29 +0400

    components

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-car',
   templateUrl: './car.component.html',
   styleUrls: ['./car.component.css'],
 })
-export class CarComponent {
-  constructor() {}
-  ngOnInit() {}
+
+// mplements OnInit дает вызвать ngOnInit, метод должен быть реализован
+export class CarComponent implements OnInit {
+  // model можно передавать в шаблон html
+  name:string = 'bmv'
+  model: string = 'i3'
+  speed: number = 20
+  colors: Icolors = {
+    car: "red",
+    salon: "red",
+    wheels: "red"
+  }
+  options: string[] = []
+  test: any
+
+  constructor() { }
+
+  // при создании компонента
+  ngOnInit() {
+    this.model = 'audi'
+    this.speed = 500
+    this.options = ["abs", "hz"]
+    this.test = 1
+    this.test = "2"
+  }
+}
+
+interface Icolors {
+  car: string,
+  salon: string,
+  wheels: string
 }
